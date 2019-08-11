482d99b4c57ca35d0e8e9912510736f69c2d0132
takumi14 <03serious@gmail.com>
2019-08-11 17:52:19 +0900

    signup function upload
    サインアップ機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
